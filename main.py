a06bc77b67c44352397f5d5257b5f384aed3a887
JanquielKappler <>
2021-10-21 00:52:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#Funcionalidades esperadas no programa:
+#a) Cadastramento de perguntas e respostas;
+#b) Apresentação das perguntas cadastradas;
+#c) Apresentação das respostas disponíveis;
+#d) Escolha de uma resposta;
+#e) Apresentação de informações sobre o resultado da escolha (correta ou incorreta);
+#f) Apresentação da quantidade de respostas corretas e pontuação total.
+
+p1 = input('Digite uma pergunta sobre Segurança da Informação: ')
+p1_r1 = input('Insira alteranativa de resposta 1: ')
+p1_r2 = input('Insira alteranativa de resposta 2: ')
+p1_r3 = input('Insira alteranativa de resposta 3: ')
+p2 = input('Digite uma pergunta sobre Segurança da Informação: ')
+p2_r1 = input('Insira alteranativa de resposta 1: ')
+p2_r2 = input('Insira alteranativa de resposta 2: ')
+p2_r3 = input('Insira alteranativa de resposta 3: ')
+p3 = input('Digite uma pergunta sobre Segurança da Informação: ')
+p3_r1 = input('Insira alteranativa de resposta 1: ')
+p3_r2 = input('Insira alteranativa de resposta 2: ')
+p3_r3 = input('Insira alteranativa de resposta 3: ')
+p4 = input('Digite uma pergunta sobre Segurança da Informação: ')
+p4_r1 = input('Insira alteranativa de resposta 1: ')
+p4_r2 = input('Insira alteranativa de resposta 2: ')
+p4_r3 = input('Insira alteranativa de resposta 3: ')
+
+print(p1)
+
