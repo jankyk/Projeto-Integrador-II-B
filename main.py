e7b23ce20543947697aacaebfd3450d228ca84f2
Janquiel Kappler <41974499+jankyk@users.noreply.github.com>
2021-10-21 00:59:52 +0000

    Inicio

--- main.py
+++ main.py
@@ -10,14 +10,18 @@ p1 = input('Digite uma pergunta sobre Segurança da Informação: ')
 p1_r1 = input('Insira alteranativa de resposta 1: ')
 p1_r2 = input('Insira alteranativa de resposta 2: ')
 p1_r3 = input('Insira alteranativa de resposta 3: ')
+respP1
+
 p2 = input('Digite uma pergunta sobre Segurança da Informação: ')
 p2_r1 = input('Insira alteranativa de resposta 1: ')
 p2_r2 = input('Insira alteranativa de resposta 2: ')
 p2_r3 = input('Insira alteranativa de resposta 3: ')
+
 p3 = input('Digite uma pergunta sobre Segurança da Informação: ')
 p3_r1 = input('Insira alteranativa de resposta 1: ')
 p3_r2 = input('Insira alteranativa de resposta 2: ')
 p3_r3 = input('Insira alteranativa de resposta 3: ')
+
 p4 = input('Digite uma pergunta sobre Segurança da Informação: ')
 p4_r1 = input('Insira alteranativa de resposta 1: ')
 p4_r2 = input('Insira alteranativa de resposta 2: ')
